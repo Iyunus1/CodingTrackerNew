3eefb1a36af7bd29ea6fd79ace1ff6ae3d372c3b
Iyunus <kelpfish@hotmail.co.uk>
2023-05-26 19:52:16 +0100

    All operand functions added giving the option to re enter numbers and calculate again

--- CodingTrackerNew/Program.cs
+++ CodingTrackerNew/Program.cs
@@ -1,28 +1,44 @@
 ﻿
-Console.WriteLine("Enter your first number");
-String read1 = Console.ReadLine();
-int num1 = Convert.ToInt32(read1);
 
-Console.WriteLine("Enter your first number");
-String read2 = Console.ReadLine();
-int num2 = Convert.ToInt32(read2);
-
-
-
-int add = num1 + num2;
-int minus = num1 - num2;
-int multiply = num1 * num2;
-double divide = num1 / num2;
 
 while(true)
 {
-    Console.WriteLine("'+'To add, '-'To minus, '*'To multiply '/'To divide  ");
+    Console.WriteLine("Enter your first number");
+    String read1 = Console.ReadLine();
+    int num1 = Convert.ToInt32(read1);
+
+    Console.WriteLine("Enter your second number");
+    String read2 = Console.ReadLine();
+    int num2 = Convert.ToInt32(read2);
+
+    int add = num1 + num2;
+    int minus = num1 - num2;
+    int multiply = num1 * num2;
+    double divide = num1 / num2;
+
+    Console.WriteLine("'+'To add, '-'To minus, '*'To multiply '/'To divide, 'quit' to ...quit  ");
     String symbol = Console.ReadLine();
-    if (symbol == "+")
+    if (symbol == "quit")
     {
-        Console.WriteLine("The sum is" + add);
+        break;
+    } else if (symbol == "+")
+    {
+        Console.WriteLine("The sum is " + add);
+        continue;
+    } else if (symbol == "-")
+    {
+        Console.WriteLine("The sum is " + minus);
+        continue;
+    } else if (symbol == "*")
+    {
+        Console.WriteLine("The sum is " + multiply);
+        continue;
+    } else if (symbol == "/")
+    {
+        Console.WriteLine("The sum is " + divide);
         continue;
     }
+
 }
 
-Console.WriteLine("The input is null");
+Console.WriteLine("Wrong input");
